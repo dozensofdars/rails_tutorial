6783687c62b436367b7abef265cb9dcf317d0229
dozensofdars <yuuya4000s@gmail.com>
2019-08-23 21:27:39 +0900

    make static pages

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す
+  # @param page_title [String] ページ固有タイトル(省略可)
+  # @return [String] 完全なタイトル
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
