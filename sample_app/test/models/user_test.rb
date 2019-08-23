b7ddc54f9fd1aa7d83144c7a6546b067c6db1abd
dozensofdars <yuuya4000s@gmail.com>
2019-08-23 23:05:34 +0900

    add test using fixtures

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:example)
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
