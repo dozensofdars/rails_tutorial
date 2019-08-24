f40e54a0915a90ebf890cd574ad72828c84c1eac
dozensofdars <yuuya4000s@gmail.com>
2019-08-24 11:45:22 +0900

    current user

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -4,4 +4,16 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # @return [User] ログイン中のユーザー
+  # @return [NilClass] 未ログイン
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
