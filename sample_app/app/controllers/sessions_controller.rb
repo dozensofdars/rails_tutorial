07ed815a5d4bf03e6a14c9fea75d6217e06c65fb
dozensofdars <yuuya4000s@gmail.com>
2019-08-24 11:12:15 +0900

    sessions#create

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # flash[:info] = 'Logged in.'
+      # redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
