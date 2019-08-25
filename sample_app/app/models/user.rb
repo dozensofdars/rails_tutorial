9ac4f33c4239a573591702adcfca77b59556e35d
dozensofdars <yuuya4000s@gmail.com>
2019-08-25 17:56:08 +0900

    create activation token

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  before_save { email.downcase! }
+
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name,
     presence: true,
     length: { maximum: 50 }
@@ -47,4 +50,16 @@ class User < ApplicationRecord
   def self.new_token
     SecureRandom.urlsafe_base64
   end
+
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      email.downcase!
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
