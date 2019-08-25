bf5dfca338d22da3bf6e712feff78d29eb56329d
dozensofdars <yuuya4000s@gmail.com>
2019-08-25 15:08:03 +0900

    フレンドリーフォワーディング

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -51,4 +51,16 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  # @param default デフォルト値のURL
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
