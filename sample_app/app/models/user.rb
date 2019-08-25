2d9c351f8bb786ab47ba009437a1ff87c223cc4d
dozensofdars <yuuya4000s@gmail.com>
2019-08-25 10:15:04 +0900

    add remember token

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save { email.downcase! }
   validates :name,
@@ -14,10 +16,21 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 }
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
   # @param string [String] 文字列
   # @return 渡された文字列のハッシュ値
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # @return [String] ランダムなトークン
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 end
