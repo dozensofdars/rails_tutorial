b6f3dd24b138d5884030f737c2612b46e8488297
dozensofdars <yuuya4000s@gmail.com>
2019-08-25 09:19:55 +0900

    add bootstrap3 + jquery

--- sample_app/config/webpack/environment.js
+++ sample_app/config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    jQuery: "jquery"
+  })
+)
+
 module.exports = environment
