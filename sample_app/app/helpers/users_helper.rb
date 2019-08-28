f5a166f2db0ad6c605548ee83123f975a4f3f4e3
dozensofdars <yuuya4000s@gmail.com>
2019-08-28 22:33:16 +0900

    gravatar_for 修正

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   # @return [String] ユーザのGravatar画像のimage_tag
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
