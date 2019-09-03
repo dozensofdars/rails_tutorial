f7893daff3f8959acde7d67930c66d67bccfbbd8
dozensofdars <yuuya4000s@gmail.com>
2019-09-03 21:52:17 +0900

    画像サイズ上限のバリデーション

--- sample_app/app/models/micropost.rb
+++ sample_app/app/models/micropost.rb
@@ -5,4 +5,11 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+
+    def picture_size
+      errors.add(:picture, "should be less than 5MB") if picture.size > 5.megabytes
+    end
 end
