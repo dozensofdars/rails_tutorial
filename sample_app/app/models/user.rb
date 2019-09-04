c7b1fddbf56b05bd23e78381fe17b7f9135ddeb4
dozensofdars <yuuya4000s@gmail.com>
2019-09-04 22:13:04 +0900

    フォローしているユーザ一覧を取得
    users --(1:n)-- relationships --(n:1)-- users
    →：following
    ←：followers

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     class_name: "Relationship",
     foreign_key: "follower_id",
     dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
